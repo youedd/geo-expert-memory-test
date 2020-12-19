c36770d710502a37f916cdc14f739a1d5fd7782e
youedd <eddibili.youssef@gmail.com>
2020-12-19 20:09:56 +0100

    initial commit

---
+++ src/data.ts
@@ -0,0 +1,706 @@
+import { t } from "ttag";
+
+const data = [
+  {
+    id: "AE",
+    title: t`United Arab Emirates`,
+  },
+  {
+    id: "AF",
+    title: t`Afghanistan`,
+  },
+  {
+    id: "AL",
+    title: t`Albania`,
+  },
+  {
+    id: "AM",
+    title: t`Armenia`,
+  },
+  {
+    id: "AO",
+    title: t`Angola`,
+  },
+  {
+    id: "AR",
+    title: t`Argentina`,
+  },
+  {
+    id: "AT",
+    title: t`Austria`,
+  },
+  {
+    id: "AU",
+    title: t`Australia`,
+  },
+  {
+    id: "AZ",
+    title: t`Azerbaijan`,
+  },
+  {
+    id: "BA",
+    title: t`Bosnia and Herzegovina`,
+  },
+  {
+    id: "BD",
+    title: t`Bangladesh`,
+  },
+  {
+    id: "BE",
+    title: t`Belgium`,
+  },
+  {
+    id: "BF",
+    title: t`Burkina Faso`,
+  },
+  {
+    id: "BG",
+    title: t`Bulgaria`,
+  },
+  {
+    id: "BI",
+    title: t`Burundi`,
+  },
+  {
+    id: "BJ",
+    title: t`Benin`,
+  },
+  {
+    id: "BN",
+    title: t`Brunei Darussalam`,
+  },
+  {
+    id: "BO",
+    title: t`Bolivia`,
+  },
+  {
+    id: "BR",
+    title: t`Brazil`,
+  },
+  {
+    id: "BS",
+    title: t`Bahamas`,
+  },
+  {
+    id: "BT",
+    title: t`Bhutan`,
+  },
+  {
+    id: "BW",
+    title: t`Botswana`,
+  },
+  {
+    id: "BY",
+    title: t`Belarus`,
+  },
+  {
+    id: "BZ",
+    title: t`Belize`,
+  },
+  {
+    id: "CA",
+    title: t`Canada`,
+  },
+  {
+    id: "CD",
+    title: t`Democratic Republic of Congo`,
+  },
+  {
+    id: "CF",
+    title: t`Central African Republic`,
+  },
+  {
+    id: "CG",
+    title: t`Republic of Congo`,
+  },
+  {
+    id: "CH",
+    title: t`Switzerland`,
+  },
+  {
+    id: "CI",
+    title: t`Côte d'Ivoire`,
+  },
+  {
+    id: "CL",
+    title: t`Chile`,
+  },
+  {
+    id: "CM",
+    title: t`Cameroon`,
+  },
+  {
+    id: "CN",
+    title: t`China`,
+  },
+  {
+    id: "CO",
+    title: t`Colombia`,
+  },
+  {
+    id: "CR",
+    title: t`Costa Rica`,
+  },
+  {
+    id: "CU",
+    title: t`Cuba`,
+  },
+  {
+    id: "CY",
+    title: t`Cyprus`,
+  },
+  {
+    id: "CZ",
+    title: t`Czechia`,
+  },
+  {
+    id: "DE",
+    title: t`Germany`,
+  },
+  {
+    id: "DJ",
+    title: t`Djibouti`,
+  },
+  {
+    id: "DK",
+    title: t`Denmark`,
+  },
+  {
+    id: "DO",
+    title: t`Dominican Republic`,
+  },
+  {
+    id: "DZ",
+    title: t`Algeria`,
+  },
+  {
+    id: "EC",
+    title: t`Ecuador`,
+  },
+  {
+    id: "EE",
+    title: t`Estonia`,
+  },
+  {
+    id: "EG",
+    title: t`Egypt`,
+  },
+  {
+    id: "ER",
+    title: t`Eritrea`,
+  },
+  {
+    id: "ES",
+    title: t`Spain`,
+  },
+  {
+    id: "ET",
+    title: t`Ethiopia`,
+  },
+  {
+    id: "FK",
+    title: t`Falkland Islands`,
+  },
+  {
+    id: "FI",
+    title: t`Finland`,
+  },
+  {
+    id: "FJ",
+    title: t`Fiji`,
+  },
+  {
+    id: "FR",
+    title: t`France`,
+  },
+  {
+    id: "GA",
+    title: t`Gabon`,
+  },
+  {
+    id: "GB",
+    title: t`United Kingdom`,
+  },
+  {
+    id: "GE",
+    title: t`Georgia`,
+  },
+  {
+    id: "GF",
+    title: t`French Guiana`,
+  },
+  {
+    id: "GH",
+    title: t`Ghana`,
+  },
+  {
+    id: "GL",
+    title: t`Greenland`,
+  },
+  {
+    id: "GM",
+    title: t`Gambia`,
+  },
+  {
+    id: "GN",
+    title: t`Guinea`,
+  },
+  {
+    id: "GQ",
+    title: t`Equatorial Guinea`,
+  },
+  {
+    id: "GR",
+    title: t`Greece`,
+  },
+  {
+    id: "GT",
+    title: t`Guatemala`,
+  },
+  {
+    id: "GW",
+    title: t`Guinea-Bissau`,
+  },
+  {
+    id: "GY",
+    title: t`Guyana`,
+  },
+  {
+    id: "HN",
+    title: t`Honduras`,
+  },
+  {
+    id: "HR",
+    title: t`Croatia`,
+  },
+  {
+    id: "HT",
+    title: t`Haiti`,
+  },
+  {
+    id: "HU",
+    title: t`Hungary`,
+  },
+  {
+    id: "ID",
+    title: t`Indonesia`,
+  },
+  {
+    id: "IE",
+    title: t`Ireland`,
+  },
+  {
+    id: "IL",
+    title: t`Israel`,
+  },
+  {
+    id: "IN",
+    title: t`India`,
+  },
+  {
+    id: "IQ",
+    title: t`Iraq`,
+  },
+  {
+    id: "IR",
+    title: t`Iran`,
+  },
+  {
+    id: "IS",
+    title: t`Iceland`,
+  },
+  {
+    id: "IT",
+    title: t`Italy`,
+  },
+  {
+    id: "JM",
+    title: t`Jamaica`,
+  },
+  {
+    id: "JO",
+    title: t`Jordan`,
+  },
+  {
+    id: "JP",
+    title: t`Japan`,
+  },
+  {
+    id: "KE",
+    title: t`Kenya`,
+  },
+  {
+    id: "KG",
+    title: t`Kyrgyzstan`,
+  },
+  {
+    id: "KH",
+    title: t`Cambodia`,
+  },
+  {
+    id: "KP",
+    title: t`North Korea`,
+  },
+  {
+    id: "KR",
+    title: t`South Korea`,
+  },
+  {
+    id: "XK",
+    title: t`Kosovo`,
+  },
+  {
+    id: "KW",
+    title: t`Kuwait`,
+  },
+  {
+    id: "KZ",
+    title: t`Kazakhstan`,
+  },
+  {
+    id: "LA",
+    title: t`Lao People's Democratic Republic`,
+  },
+  {
+    id: "LB",
+    title: t`Lebanon`,
+  },
+  {
+    id: "LK",
+    title: t`Sri Lanka`,
+  },
+  {
+    id: "LR",
+    title: t`Liberia`,
+  },
+  {
+    id: "LS",
+    title: t`Lesotho`,
+  },
+  {
+    id: "LT",
+    title: t`Lithuania`,
+  },
+  {
+    id: "LU",
+    title: t`Luxembourg`,
+  },
+  {
+    id: "LV",
+    title: t`Latvia`,
+  },
+  {
+    id: "LY",
+    title: t`Libya`,
+  },
+  {
+    id: "MA",
+    title: t`Morocco`,
+  },
+  {
+    id: "MD",
+    title: t`Moldova`,
+  },
+  {
+    id: "ME",
+    title: t`Montenegro`,
+  },
+  {
+    id: "MG",
+    title: t`Madagascar`,
+  },
+  {
+    id: "MK",
+    title: t`North Macedonia`,
+  },
+  {
+    id: "ML",
+    title: t`Mali`,
+  },
+  {
+    id: "MM",
+    title: t`Myanmar`,
+  },
+  {
+    id: "MN",
+    title: t`Mongolia`,
+  },
+  {
+    id: "MR",
+    title: t`Mauritania`,
+  },
+  {
+    id: "MW",
+    title: t`Malawi`,
+  },
+  {
+    id: "MX",
+    title: t`Mexico`,
+  },
+  {
+    id: "MY",
+    title: t`Malaysia`,
+  },
+  {
+    id: "MZ",
+    title: t`Mozambique`,
+  },
+  {
+    id: "NA",
+    title: t`Namibia`,
+  },
+  {
+    id: "NC",
+    title: t`New Caledonia`,
+  },
+  {
+    id: "NE",
+    title: t`Niger`,
+  },
+  {
+    id: "NG",
+    title: t`Nigeria`,
+  },
+  {
+    id: "NI",
+    title: t`Nicaragua`,
+  },
+  {
+    id: "NL",
+    title: t`Netherlands`,
+  },
+  {
+    id: "NO",
+    title: t`Norway`,
+  },
+  {
+    id: "NP",
+    title: t`Nepal`,
+  },
+  {
+    id: "NZ",
+    title: t`New Zealand`,
+  },
+  {
+    id: "OM",
+    title: t`Oman`,
+  },
+  {
+    id: "PA",
+    title: t`Panama`,
+  },
+  {
+    id: "PE",
+    title: t`Peru`,
+  },
+  {
+    id: "PG",
+    title: t`Papua New Guinea`,
+  },
+  {
+    id: "PH",
+    title: t`Philippines`,
+  },
+  {
+    id: "PL",
+    title: t`Poland`,
+  },
+  {
+    id: "PK",
+    title: t`Pakistan`,
+  },
+  {
+    id: "PR",
+    title: t`Puerto Rico`,
+  },
+  {
+    id: "PS",
+    title: t`Palestinian Territories`,
+  },
+  {
+    id: "PT",
+    title: t`Portugal`,
+  },
+  {
+    id: "PY",
+    title: t`Paraguay`,
+  },
+  {
+    id: "QA",
+    title: t`Qatar`,
+  },
+  {
+    id: "RO",
+    title: t`Romania`,
+  },
+  {
+    id: "RS",
+    title: t`Serbia`,
+  },
+  {
+    id: "RU",
+    title: t`Russia`,
+  },
+  {
+    id: "RW",
+    title: t`Rwanda`,
+  },
+  {
+    id: "SA",
+    title: t`Saudi Arabia`,
+  },
+  {
+    id: "SB",
+    title: t`Solomon Islands`,
+  },
+  {
+    id: "SD",
+    title: t`Sudan`,
+  },
+  {
+    id: "SE",
+    title: t`Sweden`,
+  },
+  {
+    id: "SI",
+    title: t`Slovenia`,
+  },
+  {
+    id: "SJ",
+    title: t`Svalbard and Jan Mayen`,
+  },
+  {
+    id: "SK",
+    title: t`Slovakia`,
+  },
+  {
+    id: "SL",
+    title: t`Sierra Leone`,
+  },
+  {
+    id: "SN",
+    title: t`Senegal`,
+  },
+  {
+    id: "SO",
+    title: t`Somalia`,
+  },
+  {
+    id: "SR",
+    title: t`Suriname`,
+  },
+  {
+    id: "SS",
+    title: t`South Sudan`,
+  },
+  {
+    id: "SV",
+    title: t`El Salvador`,
+  },
+  {
+    id: "SY",
+    title: t`Syria`,
+  },
+  {
+    id: "SZ",
+    title: t`Swaziland`,
+  },
+  {
+    id: "TD",
+    title: t`Chad`,
+  },
+  {
+    id: "TF",
+    title: t`French Southern and Antarctic Lands`,
+  },
+  {
+    id: "TG",
+    title: t`Togo`,
+  },
+  {
+    id: "TH",
+    title: t`Thailand`,
+  },
+  {
+    id: "TJ",
+    title: t`Tajikistan`,
+  },
+  {
+    id: "TL",
+    title: t`Timor-Leste`,
+  },
+  {
+    id: "TM",
+    title: t`Turkmenistan`,
+  },
+  {
+    id: "TN",
+    title: t`Tunisia`,
+  },
+  {
+    id: "TR",
+    title: t`Turkey`,
+  },
+  {
+    id: "TT",
+    title: t`Trinidad and Tobago`,
+  },
+  {
+    id: "TW",
+    title: t`Taiwan`,
+  },
+  {
+    id: "TZ",
+    title: t`Tanzania`,
+  },
+  {
+    id: "UA",
+    title: t`Ukraine`,
+  },
+  {
+    id: "UG",
+    title: t`Uganda`,
+  },
+  {
+    id: "US",
+    title: t`United States`,
+  },
+  {
+    id: "UY",
+    title: t`Uruguay`,
+  },
+  {
+    id: "UZ",
+    title: t`Uzbekistan`,
+  },
+  {
+    id: "VE",
+    title: t`Venezuela`,
+  },
+  {
+    id: "VN",
+    title: t`Vietnam`,
+  },
+  {
+    id: "VU",
+    title: t`Vanuatu`,
+  },
+  {
+    id: "YE",
+    title: t`Yemen`,
+  },
+  {
+    id: "ZA",
+    title: t`South Africa`,
+  },
+  {
+    id: "ZM",
+    title: t`Zambia`,
+  },
+  {
+    id: "ZW",
+    title: t`Zimbabwe`,
+  },
+];
+
+export default data;
