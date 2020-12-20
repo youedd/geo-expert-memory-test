eef57340a4612ebf9c77db6cd7368ad883d17f18
youedd <eddibili.youssef@gmail.com>
2020-12-20 15:24:01 +0100

    - remove lerp and resize on windows size change

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,60 @@
 import SvgPanZoom, { resetPan } from "svg-pan-zoom";
 import data from "./scripts/data";
+/*
+// const delta = 0.1;
 
-const delta = 0.1;
+// const lerp = (start: number, end: number, amt: number) => {
+//   return (1 - amt) * start + amt * end;
+// };
 
-const lerp = (start: number, end: number, amt: number) => {
-  return (1 - amt) * start + amt * end;
+// let newZoom;
+// let intervalId: NodeJS.Timeout | null = null;
+
+// const startLerp = () => {
+//   if (intervalId) {
+//     return;
+//   }
+//   intervalId = setInterval(() => {
+//     const zoom = panZoom.getZoom();
+//     if (zoom === newZoom) {
+//       clearInterval(intervalId);
+//       intervalId = null;
+//       return;
+//     }
+//     panZoom.zoom(lerp(zoom, newZoom, delta));
+//   }, 1000 / 60);
+// };
+*/
 };
 
-const svgHandler = SvgPanZoom("svg", {
-  dblClickZoomEnabled: false,
-  mouseWheelZoomEnabled: false,
+const panZoom = SvgPanZoom("svg", {
+  dblClickZoomEnabled: true,
+  mouseWheelZoomEnabled: true,
 });
 
+window.addEventListener("resize", () => {
+  panZoom.resize();
+  panZoom.fit();
+  panZoom.center();
+});
 
 const homeButton = document.getElementById("home");
 homeButton.addEventListener("click", () => {
-  clearInterval(intervalId);
-  intervalId= null;
-  svgHandler.reset();
-  
+  // clearInterval(intervalId);
+  // intervalId = null;
+  panZoom.reset();
 });
-
-let newZoom;
-let intervalId: NodeJS.Timeout | null = null;
-
-const startLerp = () => {
-
-  if (intervalId) {
-    return;
-  }
-  intervalId = setInterval(() => {
-    const zoom = svgHandler.getZoom();
-    if (zoom === newZoom) {
-      clearInterval(intervalId);
-      intervalId= null;
-      return;
-    }
-    svgHandler.zoom(lerp(zoom, newZoom, delta));
-  }, 1000 / 60);
-}
 
 const zoominButoon = document.getElementById("zoomin");
 zoominButoon.addEventListener("click", () => {
-  newZoom = svgHandler.getZoom() * 1.5;
-  startLerp();
+  const newZoom = panZoom.getZoom() * 1.5;
+  panZoom.zoom(newZoom);
 });
 
 const zoomoutButoon = document.getElementById("zoomout");
 zoomoutButoon.addEventListener("click", () => {
-  newZoom = svgHandler.getZoom() / 1.5;
-  startLerp();
+  const newZoom = panZoom.getZoom() / 1.5;
+  panZoom.zoom(newZoom);
 });
 
 
